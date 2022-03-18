fee59117fcf226e4d69c4fd08ff4f6f63dcd13e4
souviks72 <souvikmandalvit2016@gmail.com>
2022-03-18 23:58:37 +0530

    User sign up created with hashed password

--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 
@@ -16,11 +19,41 @@ func (U *UserHandler) CreateUser(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{"msg": "Please send proper request body"})
 	}
 
-	res, err :=U.UserCollection.InsertOne(context.Background(),user)
+	ctx := context.Background()
+
+	var userExist User
+	searchErr := U.UserCollection.FindOne(ctx,bson.M{"name": user.Name}).Decode(&userExist)
+	if searchErr != nil && searchErr != mongo.ErrNoDocuments{ 
+		fmt.Println(searchErr)
+		return err  
+	}else if searchErr == nil && userExist.Name == user.Name{
+		return c.JSON(http.StatusAlreadyReported, "User exists, log in to continue using")
+	}
+
+	user.Password, _ = HashPassword(user.Password)
+
+	res, err := U.UserCollection.InsertOne(ctx,user)
 	if err != nil{
 		fmt.Println("CreateUser Insertion Failed %+v\n",err)
-		return echo.NewHTTPError(http.StatusInternalServerError, "DB insertion failed")
+		return c.JSON(http.StatusInternalServerError, "DB insertion failed")
 	}
 	fmt.Println(res)
 	return c.JSON(http.StatusCreated, user)
+}
+
+func HashPassword(password string) (string, error) {
+	pwd := []byte(password)
+	res, err := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+	if err != nil{
+		return "", err
+	}
+
+	return string(res), nil
+}
+
+func ComparePassword(hashedPassword string, password string) error {
+	pwd := []byte(password)
+	hash := []byte(hashedPassword)
+	err := bcrypt.CompareHashAndPassword(hash, pwd)
+	return err
 }
